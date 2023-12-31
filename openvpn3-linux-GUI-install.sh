ce0d2c44647f71b0309ab017f1f0cd272902aa43
malenkiya <64140489+malenkiya@users.noreply.github.com>
2023-12-31 03:56:11 +0200

    Update openvpn3-linux-GUI-install.sh

--- openvpn3-linux-GUI-install.sh
+++ openvpn3-linux-GUI-install.sh
@@ -1,13 +1,13 @@
 DISTRO=focal
 
 
-sudo apt-get install curl wget
+sudo apt-get -y install curl wget
 sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc
 sudo rm etc/apt/sources.list.d/openvpn-packages.list
 echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $DISTRO main" | sudo tee /etc/apt/sources.list.d/openvpn-packages.list 
-sudo apt update
-sudo apt install openvpn3
-sudo apt install openvpn-dco-dkms 
+sudo apt -y update
+sudo apt install -y openvpn3
+sudo apt install -y openvpn-dco-dkms 
 wget https://github.com/malenkiya/openvpn3-linux-GUI/raw/main/openvpn-saml.deb
 sudo dpkg -i openvpn-saml.deb
 
