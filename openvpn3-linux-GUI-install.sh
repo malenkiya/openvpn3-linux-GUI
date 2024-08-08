358a4965f22e4488d7348e1192def9d151d5786e
malenkiya <64140489+malenkiya@users.noreply.github.com>
2024-08-08 23:02:33 +0300

    Update openvpn3-linux-GUI-install.sh

--- openvpn3-linux-GUI-install.sh
+++ openvpn3-linux-GUI-install.sh
@@ -1,14 +1,34 @@
-DISTRO=focal
+#!/bin/bash
 
+# Detect the distribution code name
+DISTRO=$(lsb_release -cs)
 
+# Check if the distribution is supported
+if [[ "$DISTRO" != "focal" && "$DISTRO" != "jammy" ]]; then
+  echo "Warning: Unsupported distribution $DISTRO. Proceeding with installation, but it may not work as expected."
+fi
+
+# Install necessary packages
 sudo apt-get -y install curl wget
-sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc
-sudo rm etc/apt/sources.list.d/openvpn-packages.list
-echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $DISTRO main" | sudo tee /etc/apt/sources.list.d/openvpn-packages.list 
+
+# Set up OpenVPN3 repository
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc
+
+# Remove existing OpenVPN sources list if it exists
+sudo rm -f /etc/apt/sources.list.d/openvpn-packages.list
+
+# Add OpenVPN repository
+echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $DISTRO main" | sudo tee /etc/apt/sources.list.d/openvpn-packages.list
+
+# Update package lists
 sudo apt -y update
+
+# Install required packages
+sudo apt install -y libxcb-cursor0
 sudo apt install -y openvpn3
-sudo apt install -y openvpn-dco-dkms 
-wget https://github.com/malenkiya/openvpn3-linux-GUI/releases/download/v.0.2/openvpn-saml.deb
+sudo apt install -y openvpn-dco-dkms
+
+# Download and install the OpenVPN GUI package
+wget https://github.com/malenkiya/openvpn3-linux-GUI/releases/download/0.3/openvpn-saml.deb
 sudo dpkg -i openvpn-saml.deb
-
-
